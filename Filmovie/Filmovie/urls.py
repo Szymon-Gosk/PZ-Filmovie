2dcccc993b331c1affc407b4bd8c1892b082feba
Damian <damian.gorski1@wp.pl>
2020-11-12 15:07:23 +0100

    working actors_view connected with movies

--- Filmovie/Filmovie/urls.py
+++ Filmovie/Filmovie/urls.py
@@ -17,4 +17,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movie/', include('movies.urls')),
+    path('actors/', include('actors.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
