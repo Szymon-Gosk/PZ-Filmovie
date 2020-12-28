8c315380a75db1a7518979857a60dc1b1f3b6745
Damian <damian.gorski1@wp.pl>
2020-12-28 17:36:29 +0100

    Links, movie details functions upgraded

--- Filmovie/movies/views.py
+++ Filmovie/movies/views.py
@@ -62,6 +62,16 @@ def movie_detail_view(request, imdb_id):
         opinions = MovieRating.objects.filter(movie=movie_data)
         user = request.user
 
+        if movie_data in user.profile.watchedlist.all():
+            watchedlist = True
+        else:
+            watchedlist = False
+
+        if movie_data in user.profile.watchlist.all():
+            watchlist = True
+        else:
+            watchlist = False
+
         if movie_data in user.profile.star.all():
             star = True
         else:
@@ -83,6 +93,8 @@ def movie_detail_view(request, imdb_id):
             'rating_avg': rating_avg,
             'rating_count': rating_count,
             "star": star,
+            "watchlist": watchlist,
+            "watchedlist": watchedlist,
         }
 
 
@@ -196,7 +208,7 @@ def genres_view(request, genre_slug):
 
     movies_for_pagination = Movie.objects.filter(Genre=genre)
 
-    paginator = Paginator(movies_for_pagination, 3)
+    paginator = Paginator(movies_for_pagination, 9)
 
     page_number = request.GET.get('page')
     movie_data = paginator.get_page(page_number)
@@ -204,6 +216,26 @@ def genres_view(request, genre_slug):
     context = {
         'movie_data': movie_data,
         'genre': genre,
+    }
+
+    template = loader.get_template('movies/genre.html')
+
+    return HttpResponse(template.render(context, request))
+
+def type_view(request, type):
+    """Returning the genres_view which renders the genre template.
+    Using paginator for the data from database"""
+
+    movies_for_pagination = Movie.objects.filter(Type=type)
+
+    paginator = Paginator(movies_for_pagination, 9)
+
+    page_number = request.GET.get('page')
+    movie_data = paginator.get_page(page_number)
+
+    context = {
+        'movie_data': movie_data,
+        'genre': type,
     }
 
     template = loader.get_template('movies/genre.html')
@@ -235,7 +267,10 @@ def add_to_watchlist_view(request, imdb_id):
     user = request.user
     profile = Profile.objects.get(user=user)
 
-    profile.watchlist.add(movie)
+    if (movie in profile.watchlist.all()):
+        profile.watchlist.remove(movie)
+    else:
+        profile.watchlist.add(movie)
 
     return HttpResponseRedirect(reverse('movie-details', args=[imdb_id]))
 
@@ -247,11 +282,14 @@ def add_to_watchedlist_view(request, imdb_id):
     user = request.user
     profile = Profile.objects.get(user=user)
 
-    if profile.watchlist.filter(imdbID=imdb_id).exists():
-        profile.watchlist.remove(movie)
-        profile.watchedlist.add(movie)
+    if movie in profile.watchedlist.all():
+        profile.watchedlist.remove(movie)
     else:
-        profile.watchedlist.add(movie)
+        if profile.watchlist.filter(imdbID=imdb_id).exists():
+            profile.watchlist.remove(movie)
+            profile.watchedlist.add(movie)
+        else:
+            profile.watchedlist.add(movie)
 
     return HttpResponseRedirect(reverse('movie-details', args=[imdb_id]))
 
