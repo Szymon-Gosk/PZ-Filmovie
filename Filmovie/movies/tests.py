84eec197fe2f127acf10bc619ad63d0a68ac4089
Damian Górski <damian.gorski1@wp.pl>
2021-03-24 19:25:12 +0100

    Partly done unit tests for Profie related stuff

--- Filmovie/movies/tests.py
+++ Filmovie/movies/tests.py
@@ -1,2 +1,34 @@
 """Test cases for movies app"""
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import MovieRating, Movie, Likes
+
+User = get_user_model()
+
+
+class LikesTestCase(TestCase):
+
+    def setUp(self):
+        self.user1 = User.objects.create(username="test1", password="test1")
+        self.user2 = User.objects.create(username="test2", password="test2")
+        self.user3 = User.objects.create(username="test3", password="test3")
+        self.movie = Movie.objects.create(Title="test", Poster='', Poster_url='')
+        self.rating = MovieRating.objects.create(user=self.user1, movie = self.movie, rate=2)
+        
+    def test_liking(self):
+        first = self.user1
+        second = self.user2
+        third = self.user3
+        rating = self.rating
+        Likes.objects.create(user=second, rating=rating, like_type=1)
+        Likes.objects.create(user=third, rating=rating, like_type=1)
+        self.assertEqual(Likes.objects.filter(rating=rating, like_type=1).count(), 2)
+        
+    def test_disliking(self):
+        first = self.user1
+        second = self.user2
+        third = self.user3
+        rating = self.rating
+        Likes.objects.create(user=second, rating=rating, like_type=2)
+        Likes.objects.create(user=third, rating=rating, like_type=2)
+        self.assertEqual(Likes.objects.filter(rating=rating, like_type=2).count(), 2)
--- Filmovie/users/tests.py
+++ Filmovie/users/tests.py
@@ -1,7 +1,10 @@
 """Test cases for users app"""
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.urls import reverse
 from .models import Profile
+from django.test import Client
 
 User = get_user_model()
 
@@ -11,17 +14,88 @@ class ProfileTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create(username="test1", password="test1")
         self.user2 = User.objects.create(username="test2", password="test2")
+        self.client = Client()
+        self.user = User.objects.create_user('test_user', password='test_user')
+        
+    def test_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_follow_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/follow' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_followers_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/followers' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_following_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/following' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_star_movies_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/star-movies' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_star_series_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/star-series' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_watchlist_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/watchlist' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
+        
+    def test_watchedlist_profile_url(self):
+        self.client.login(username='test_user', password='test_user')
+        res = self.client.get('/user/%s/watchedlist' % (self.user1.username), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="profiles/profile.html")
         
     def test_profile_created_via_signal(self):
         qs = Profile.objects.all()
-        self.assertEqual(qs.count(), 2)
+        self.assertEqual(qs.count(), 3)
         
     def test_following(self):
         first = self.user1
         second = self.user2
-        # first_profile = first.profile.followers.add(second)
+        first_profile = first.profile.followers.add(second)
         second_user_following_whom = second.following.all()
         qs = second_user_following_whom.filter(user=first)
         first_user_following_no_one = first.following.all()
         self.assertTrue(qs.exists())
         self.assertFalse(first_user_following_no_one.exists())
+        
+
+class SignUpTest(TestCase):
+    def setUp(self) -> None:
+        self.username = 'test'
+        self.first_name = 'test'
+        self.last_name = 'test'
+        self.email = 'test@test.pl'
+        self.password = 'Password123!@#'
+        self.confirm_password = 'Password123!@#'
+        
+    def test_login_url(self):
+        res = self.client.get(reverse('login'), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="auth/login.html")
+        
+    def test_signup_url(self):
+        res = self.client.get(reverse('signup'), follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, template_name="auth/register.html")
+        
